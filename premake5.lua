74527fd8cb442d358821190f8b5f6662b3766e04
OKAY808 <georgedautov808@gmail.com>
2022-05-03 02:03:32 +0300

    updated premake5.lua

--- premake5.lua
+++ premake5.lua
@@ -3,84 +3,78 @@ project "Freetype"
     language "C"
     staticruntime "on"
 
+    targetdir ("%{prj.location}/bin/" .. outputdir)
+    objdir ("%{prj.location}/bin-int/" .. outputdir)
+
+    files 
+    { 
+        "include/ft2build.h",
+		"include/freetype/*.h",
+		"include/freetype/config/*.h",
+		"include/freetype/internal/*.h",
+
+		"src/autofit/autofit.c",
+		"src/base/ftbase.c",
+		"src/base/ftbbox.c",
+		"src/base/ftbdf.c",
+		"src/base/ftbitmap.c",
+		"src/base/ftcid.c",
+		"src/base/ftdebug.c",
+		"src/base/ftfstype.c",
+		"src/base/ftgasp.c",
+		"src/base/ftglyph.c",
+		"src/base/ftgxval.c",
+		"src/base/ftinit.c",
+		"src/base/ftmm.c",
+		"src/base/ftotval.c",
+		"src/base/ftpatent.c",
+		"src/base/ftpfr.c",
+		"src/base/ftstroke.c",
+		"src/base/ftsynth.c",
+		"src/base/ftsystem.c",
+		"src/base/fttype1.c",
+		"src/base/ftwinfnt.c",
+		"src/bdf/bdf.c",
+		"src/bzip2/ftbzip2.c",
+		"src/cache/ftcache.c",
+		"src/cff/cff.c",
+		"src/cid/type1cid.c",
+		"src/gzip/ftgzip.c",
+		"src/lzw/ftlzw.c",
+		"src/pcf/pcf.c",
+		"src/pfr/pfr.c",
+		"src/psaux/psaux.c",
+		"src/pshinter/pshinter.c",
+		"src/psnames/psnames.c",
+		"src/raster/raster.c",
+		"src/sdf/sdf.c",
+		"src/sfnt/sfnt.c",
+		"src/smooth/smooth.c",
+		"src/truetype/truetype.c",
+		"src/type1/type1.c",
+		"src/type42/type42.c",
+		"src/winfonts/winfnt.c"
+
+    }
+
     includedirs 
     { 
         "include" 
     }
-    
-    files 
-    { 
-        "src/autofit/autofit.c",
-        "src/base/ftbase.c",
-        "src/base/ftbbox.c",
-        "src/base/ftbdf.c",
-        "src/base/ftbitmap.c",
-        "src/base/ftcid.c",
-        "src/base/ftfstype.c",
-        "src/base/ftgasp.c",
-        "src/base/ftglyph.c",
-        "src/base/ftgxval.c",
-        "src/base/ftinit.c",
-        "src/base/ftmm.c",
-        "src/base/ftotval.c",
-        "src/base/ftpatent.c",
-        "src/base/ftpfr.c",
-        "src/base/ftstroke.c",
-        "src/base/ftsynth.c",
-        "src/base/ftsystem.c",
-        "src/base/fttype1.c",
-        "src/base/ftwinfnt.c",
-        "src/bdf/bdf.c",
-        "src/cache/ftcache.c",
-        "src/cff/cff.c",
-        "src/cid/type1cid.c",
-        "src/gzip/ftgzip.c",
-        "src/lzw/ftlzw.c",
-        "src/pcf/pcf.c",
-        "src/pfr/pfr.c",
-        "src/psaux/psaux.c",
-        "src/pshinter/pshinter.c",
-        "src/psnames/psmodule.c",
-        "src/raster/raster.c",
-        "src/sfnt/sfnt.c",
-        "src/smooth/smooth.c",
-        "src/truetype/truetype.c",
-        "src/type1/type1.c",
-        "src/type42/type42.c",
-        "src/winfonts/winfnt.c",
-        "src/sdf/sdf.c"
-
-    }
 
     defines 
     { 
-        "FT2_BUILD_LIBRARY" 
+        "FT2_BUILD_LIBRARY",
+        "_CRT_SECURE_NO_WARNINGS"
     }
     
     filter "system:windows"
     systemversion "latest"
     
-    files 
-    {
-        "builds/windows/ftdebug.c"
-    }
-    
-    
     filter "configurations:Debug"
-        defines 
-        {
-            "FT_DEBUG_LEVEL_ERROR",
-            "FT_DEBUG_LEVEL_TRACE"
-        }
-        
         runtime "Debug"
         symbols "on"
       
       filter "configurations:Release"
-        defines
-        {
-            "NDEBUG"
-        }
-        
         runtime "Release"
         optimize "on"
